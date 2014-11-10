2ea80445a6d4c9e6745f8c989c671da0aea0ee88
mam1 <mam1@mam1-Macmini>
2014-11-10 17:40:42 -0500

    problem

--- echo-1.c
+++ echo-1.c
@@ -1,31 +1,52 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include "echo-1.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
 
-#define	ESC		27
+int main(int argc,char** argv)
+{
+        struct termios tio;
+        struct termios stdio;
+        struct termios old_stdio;
+        int tty_fd;
 
+        unsigned char c='D';
+        tcgetattr(STDOUT_FILENO,&old_stdio);
 
-int main (int argc, char *argv[]) {
-	char c;
-	char		input_buffer[INPUT_BUFFER_SIZE],*i;
+        printf("Please start with %s /dev/ttyS1 (for example)\n",argv[0]);
+        memset(&stdio,0,sizeof(stdio));
+        stdio.c_iflag=0;
+        stdio.c_oflag=0;
+        stdio.c_cflag=0;
+        stdio.c_lflag=0;
+        stdio.c_cc[VMIN]=1;
+        stdio.c_cc[VTIME]=0;
+        tcsetattr(STDOUT_FILENO,TCSANOW,&stdio);
+        tcsetattr(STDOUT_FILENO,TCSAFLUSH,&stdio);
+        fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);       // make the reads non-blocking
 
-	printf("\n **** echo-1  ver 0.0.1 active\n\n");
-	printf("enter >");
+        memset(&tio,0,sizeof(tio));
+        tio.c_iflag=0;
+        tio.c_oflag=0;
+        tio.c_cflag=CS8|CREAD|CLOCAL;           // 8n1, see termios.h for more information
+        tio.c_lflag=0;
+        tio.c_cc[VMIN]=1;
+        tio.c_cc[VTIME]=5;
 
-	i = input_buffer;
-	/* use system call to make terminal send all keystrokes directly to stdin */
-	system ("/bin/stty raw");
+        tty_fd=open("/dev/tty1", O_RDWR | O_NONBLOCK);
+        cfsetospeed(&tio,B9600);
+        cfsetispeed(&tio,B9600);
+        tcsetattr(tty_fd,TCSANOW,&tio);
+        while (c!='q')
+        {
+                if (read(tty_fd,&c,1)>0)        write(STDOUT_FILENO,&c,1);              // if new data is available on the serial port, print it out
+                if (read(STDIN_FILENO,&c,1)>0)  write(tty_fd,&c,1);                     // if new data is available on the console, send it to the serial port
+        }
 
-	while((c=getchar()!= ESC)) {
-	  /* type a period to break out of the loop, since CTRL-D won't work raw */
-	  *i++=c;
+        close(tty_fd);
+        tcsetattr(STDOUT_FILENO,TCSANOW,&old_stdio);
 
-	}
-	*i = '\0';
-	printf("\n<%s>",input_buffer);
-	/* use system call to set terminal behavior to more normal behavior */
-	system ("/bin/stty cooked");
-	printf("\n\nnormal termination\n");
-    return 0;
+        return EXIT_SUCCESS;
 }
